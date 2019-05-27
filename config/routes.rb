6334926fa43118ad67506f08f34fbe2488a3a7cb
Mateus Veloso <mfvelosos@gmail.com>
2019-05-26 22:58:43 -0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
 
-      resources :users, param: :_username
+      resources :users, param: :username
       post '/auth/login', to: 'authentication#login'
       get '/*a', to: 'api#not_found'
 
