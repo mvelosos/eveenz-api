f826507ab99e5cc7d68dde77459ba5e61e2b7729
Mateus Veloso <mfvelosos@gmail.com>
2019-05-27 00:42:23 -0300

    Update jwt expiration time

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,14 +1,14 @@
 module Api
   module V1
     class AuthenticationController < ApplicationController
-      before_action :authenticate_by_token, except: :login
+      before_action :authenticate_by_token, except: [:login]
 
       # POST /auth/login
       def login
         @user = find_by_username_or_email
         if @user && @user.authenticate(login_params[:password])
           token = JsonWebToken.encode(user_id: @user.id)
-          time = Time.now + 24.hours.to_i
+          time = Time.now + Settings.JWT_EXPIRATION_TIME.hours.to_i
           render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"), username: @user.username }, status: :ok
         else
           render json: { error: 'unauthorized' }, status: :unauthorized
--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,9 +1,10 @@
 class JsonWebToken
 
   SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
+  EXPIRATION_TIME = Settings.JWT_EXPIRATION_TIME
 
-  def self.encode(payload, exp = 24.hours.from_now)
-    payload[:exp] = exp.to_i
+  def self.encode(payload)
+    payload[:exp] = EXPIRATION_TIME.hours.to_i
     JWT.encode(payload, SECRET_KEY)
   end
 
