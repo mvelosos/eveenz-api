3a8e66b47a054e38556a8466524cce4f21d3a598
Mateus Veloso <mfvelosos@gmail.com>
2019-05-27 01:14:04 -0300

    Disable jwt exp time

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
 
-      resources :users, param: :username
+      resources :users, param: :username, only: [:show, :create, :update]
       post '/auth/login', to: 'authentication#login'
       get '/*a', to: 'api#not_found'
 
