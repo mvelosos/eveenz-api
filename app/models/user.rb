659a8f499cee8111a358a87d2f32f9935c58397e
Mateus Veloso <mfvelosos@gmail.com>
2019-05-30 02:14:48 -0300

    Model annotations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,9 @@
 #  password_digest :string
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
+#  uid             :string
+#  provider        :string
+#  active          :boolean          default(TRUE)
 #
 
 class User < ApplicationRecord
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string
+#  email           :string
+#  password_digest :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  uid             :string
+#  provider        :string
+#  active          :boolean          default(TRUE)
+#
+
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :email, :created_at
 end
