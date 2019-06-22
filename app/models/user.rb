0f324e9b247d7de7e62d4acc83e43257ae051a52
Mateus Veloso <mfvelosos@gmail.com>
2019-06-22 11:48:18 -0300

    Add validations to username

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,9 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :username, presence: true, uniqueness: true
-  validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+  validates :email,     presence: true, uniqueness: true
+  validates :email,     format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :username,  presence: true, uniqueness: true
+  validates :username,  format: { with: /\A[a-zA-Z0-9_.]+\Z/ }
+  validates :password,  length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
 end
