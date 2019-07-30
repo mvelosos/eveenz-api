076029e070f2d92b6d70e4adc00f479742b77aa1
Mateus Veloso <mfvelosos@gmail.com>
2019-07-30 02:24:48 -0300

    Disabling exp date, for now...

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -10,7 +10,7 @@ module Api
     
       def authenticate_by_token
         token = request.headers['Authorization']
-        # token = token.split(' ').last if token
+        token = token.split(' ').last if token
         begin
           @decoded_token = JsonWebToken.decode(token)
           @current_user = User.find(@decoded_token[:user_id])
