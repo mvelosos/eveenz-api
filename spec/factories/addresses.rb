921a940e6d407f4d69372cd7b9a129b264b84409
Mateus Veloso <mfvelosos@gmail.com>
2019-08-24 20:48:19 -0300

    Create address model and set associations

---
+++ spec/factories/addresses.rb
@@ -0,0 +1,32 @@
+# == Schema Information
+#
+# Table name: addresses
+#
+#  id               :bigint           not null, primary key
+#  addressable_type :string
+#  addressable_id   :bigint
+#  street           :string
+#  number           :string
+#  complement       :string
+#  neighborhood     :string
+#  zip_code         :string
+#  city             :string
+#  state            :string
+#  country          :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
+FactoryBot.define do
+  factory :address do
+    addressable { nil }
+    street { "MyString" }
+    number { "MyString" }
+    complement { "MyString" }
+    neighborhood { "MyString" }
+    zip_code { "MyString" }
+    city { "MyString" }
+    state { "MyString" }
+    country { "MyString" }
+  end
+end
---
+++ spec/models/address_spec.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: addresses
+#
+#  id               :bigint           not null, primary key
+#  addressable_type :string
+#  addressable_id   :bigint
+#  street           :string
+#  number           :string
+#  complement       :string
+#  neighborhood     :string
+#  zip_code         :string
+#  city             :string
+#  state            :string
+#  country          :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
+require 'rails_helper'
+
+RSpec.describe Address, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
