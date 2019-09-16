0397a20ed2e03c2465aae0d12a4279b375d5cde6
Mateus Veloso <mfvelosos@gmail.com>
2019-09-16 03:15:55 -0300

    Add events controller to modules path

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,10 +1,15 @@
-class Api::V1::EventsController < ApplicationController
+module Api
+  module V1
+    class Api::V1::EventsController < ApiController
 
-  # TODO: CONTINUAR O DESENVOLVIMENTO DESSE METODO
-  # GET /events
-  def index
-    @events = Event.all
-    render json: @events, status: :ok
+      # TODO: CONTINUAR O DESENVOLVIMENTO DESSE METODO
+      # TODO: Adicionar Haversine
+      # GET /events
+      def index
+        @events = Event.all
+        render json: @events, status: :ok
+      end
+    
+    end 
   end
-
 end
