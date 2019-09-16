c6cbe297b167557a023a111e296f513b3b2833fa
Mateus Veloso <mfvelosos@gmail.com>
2019-09-16 01:35:55 -0300

    Add annotations

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,8 +1,9 @@
 class Api::V1::EventsController < ApplicationController
 
+  # TODO: CONTINUAR O DESENVOLVIMENTO DESSE METODO
+  # GET /events
   def index
     @events = Event.all
-
     render json: @events, status: :ok
   end
 
--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id          :bigint           not null, primary key
+#  account_id  :bigint
+#  name        :string
+#  description :text
+#  active      :boolean          default(TRUE)
+#  kind        :string
+#  date        :date
+#  time        :time
+#  tags        :text             default([]), is an Array
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class EventSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
