3a2f8ce280b5c7af837d69a2a6d4d8a920a45a9d
Mateus Veloso <mfvelosos@gmail.com>
2019-10-15 02:58:53 -0300

    Development of event create action

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -12,7 +12,11 @@ module Api
 
       # POST /events
       def create
-        p event_params
+        @event = current_user.account.events.build(event_params)
+        p @event
+        p @event.address
+        p @event.localization
+        # @event.save
       end
 
       # GET /events/mine
@@ -32,7 +36,21 @@ module Api
 
       private
         def event_params
-          params.require(:event).permit(:name, :description, :date, :time)
+          params.require(:event).permit(:name, :description, :date, :time,  images: [:uri, :type, :name], :address_attributes => address_attr, :localization_attributes => localization_attr)
+        end
+
+        def address_attr
+          [:number, :street ,:neighborhood ,:city ,:state ,:country ,:zip_code]
+        end
+
+        def localization_attr
+          [:latitude, :longitude]
+        end
+
+        def stores_event_images()
+          decoded_image = Base64.decode64(event_params['images'][0]['uri'])
+          a = Account.first
+          a.avatar.attach(io: StringIO.new(decoded_image), filename: event_params['images'][0]['name'])
         end
     
     end 
