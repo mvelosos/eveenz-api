a05cbdfa4f20107e51891fdd08f6c65e4cd83bcb
Mateus Veloso <mfvelosos@gmail.com>
2019-10-16 04:28:42 -0300

    test sentry

--- config/sentry.rb
+++ config/sentry.rb
@@ -1,4 +1,4 @@
 Raven.configure do |config|
-  config.dsn = ENV['SENTRY_DSN']
+  config.dsn = 'https://be371897990b45c8946172ede69a1d37:43c72917559a4230b19557b10db8734c@sentry.io/1781153'
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
 end
