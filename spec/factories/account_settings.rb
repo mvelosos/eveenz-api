c2028e63fdb1da7da8c8314131b169c2e1b077a8
Mateus Veloso <mfvelosos@gmail.com>
2019-10-22 03:08:54 -0300

    .

---
+++ spec/factories/account_settings.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: account_settings
+#
+#  id              :bigint           not null, primary key
+#  account_id      :bigint
+#  distance_radius :float            default(10.0), not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  unit            :string           default("km"), not null
+#
+
+FactoryBot.define do
+  factory :account_setting do
+    account { nil }
+    event_range { 1.5 }
+  end
+end
