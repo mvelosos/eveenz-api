d1280372883b5e9b524fbc50be9527eb128cb45a
Mateus Veloso <mfvelosos@gmail.com>
2019-10-22 03:09:01 -0300

    .

---
+++ app/models/account_setting.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: account_settings
+#
+#  id              :bigint           not null, primary key
+#  account_id      :bigint
+#  distance_radius :float            default(10.0), not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  unit            :string           default("km"), not null
+#
+
+class AccountSetting < ApplicationRecord
+  belongs_to :account
+
+  validates :distance_radius, presence: true
+  validates :unit,            presence: true
+  validates :unit,            inclusion: %w(km mi)
+end
---
+++ spec/models/account_setting_spec.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: account_settings
+#
+#  id              :bigint           not null, primary key
+#  account_id      :bigint
+#  distance_radius :float            default(10.0), not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  unit            :string           default("km"), not null
+#
+
+require 'rails_helper'
+
+RSpec.describe AccountSetting, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
