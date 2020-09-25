64f552c7184a86f5fdf5c0b7254a2768d9ef2eae
Mateus Veloso <mfvelosos@gmail.com>
2020-09-25 10:55:25 -0300

    .

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,49 +1,45 @@
-module Api
-  module V1
-    class AccountsController < Api::V1::ApiController
-      before_action :set_account
+class Api::V1::AccountsController < Api::V1::ApiController
+  before_action :set_account
 
-      # GET /accounts
-      def index
-        render json: @account, status: :ok
-      end
+  # GET /accounts
+  def index
+    render json: @account, status: :ok
+  end
 
-      # PATCH /accounts
-      def update
-        if @account.update(account_params)
-          render json: '', status: :no_content
-        else
-          render json: { error: @account.errors.full_messages }, status: :not_acceptable
-        end
-      end
-
-      # GET /accounts/following
-      def following
-        render json: @account.following_by_type('Account'), status: :ok
-      end
-
-      # GET /accounts/following
-      def followers
-        render json: @account.followers_by_type('Account'), status: :ok
-      end
-
-      private
-
-      def account_params
-        params.require(:account).permit(:name, :bio, :latitude, :longitude, localization_attributes: localization_attr, address_attributes: address_attr)
-      end
-
-      def localization_attr
-        %i[latitude longitude]
-      end
-
-      def address_attr
-        %i[street number complement neighborhood zip_code city state country]
-      end
-
-      def set_account
-        @account = current_user.account
-      end
+  # PATCH /accounts
+  def update
+    if @account.update(account_params)
+      render json: '', status: :no_content
+    else
+      render json: { error: @account.errors.full_messages }, status: :not_acceptable
     end
+  end
+
+  # GET /accounts/following
+  def following
+    render json: @account.following_by_type('Account'), status: :ok
+  end
+
+  # GET /accounts/following
+  def followers
+    render json: @account.followers_by_type('Account'), status: :ok
+  end
+
+  private
+
+  def account_params
+    params.require(:account).permit(:name, :bio, :latitude, :longitude, localization_attributes: localization_attr, address_attributes: address_attr)
+  end
+
+  def localization_attr
+    %i[latitude longitude]
+  end
+
+  def address_attr
+    %i[street number complement neighborhood zip_code city state country]
+  end
+
+  def set_account
+    @account = current_user.account
   end
 end
