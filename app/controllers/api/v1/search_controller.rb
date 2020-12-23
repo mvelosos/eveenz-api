dbde22cc7d6d1646cf03a7749085de1baa58e1a2
Mateus Veloso <mfvelosos@gmail.com>
2020-12-23 20:00:47 -0300

    updated search_controller to use search_service

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,22 +1,7 @@
 class Api::V1::SearchController < Api::V1::ApiController
   # GET /search?query=
   def index
-    users    = User.search(params[:query], page: (params[:page] || 1))
-    accounts = Account.search(params[:query], page: (params[:page] || 1))
-    events   = Event.search(params[:query], page: (params[:page] || 1))
-
-    accounts_from_users = []
-    users.each do |user|
-      accounts_from_users << user.account
-    end
-
-    accounts = accounts.to_a.concat(accounts_from_users).uniq
-
-    accounts_json = ActiveModelSerializers::SerializableResource.new(accounts, each_serializer: AccountSerializer)
-    events_json   = ActiveModelSerializers::SerializableResource.new(events,
-                                                                     each_serializer: EventSerializer,
-                                                                     current_user: current_user)
-
-    render json: { users: accounts_json, events: events_json }, status: :ok
+    result = Api::V1::Search::SearchService.call(current_user.account, params)
+    render json: result, status: :ok
   end
 end
