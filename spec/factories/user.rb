64128b1e0ee7d531d86b1238f30e8b468fdab86e
Mateus Veloso <mfvelosos@gmail.com>
2020-12-27 20:25:22 -0300

    annotations

--- spec/factories/user.rb
+++
@@ -1,8 +0,0 @@
-# User Factory
-FactoryBot.define do
-  factory :user do
-    username { Faker::Lorem.word }
-    email { Faker::Internet.free_email }
-    password { Faker::Internet.password }
-  end
-end
