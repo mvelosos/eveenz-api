18d14d5a6f9c6f30b777f5a94ff2c07ef84b9245
Mateus Veloso <mfvelosos@gmail.com>
2020-12-27 21:29:03 -0300

    update constraints on accounts_controller show

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
 
-      resources :accounts, param: :username, only: %i[show]
+      resources :accounts, param: :username, only: %i[show], constraints: { username: /[0-z\.]+/ }
       resources :events, only: [:index, :create]
       resources :search, only: [:index]
       resources :users, only: %i[create]
