8235ad93a4771fdbb79c0af944ef0269224366cc
Mateus Veloso <mfvelosos@gmail.com>
2021-01-14 17:22:09 -0300

    created password recovery feature

--- app/controllers/api/v1/passwords_controller.rb
+++ app/controllers/api/v1/passwords_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::PasswordsController < Api::V1::ApiController
-  before_action :authenticate_by_token, except: %i[forgot update]
+  before_action :authenticate_by_token, except: %i[forgot verify_code recover_password]
   before_action :user, only: %i[forgot]
 
   def forgot
@@ -7,21 +7,54 @@ class Api::V1::PasswordsController < Api::V1::ApiController
     render json: { valid: true, email: @user.email }, status: :ok
   end
 
-  def verify_code; end
+  def verify_code
+    @password_recovery = PasswordRecovery.find_by_code!(verify_code_params[:code])
+    @password_recovery.update(token: SecureRandom.hex(16)) if @password_recovery.token.nil?
+    render json: { valid: true, token: @password_recovery.token }, status: :accepted
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'O código de verificação informado não é válido!' }, status: :bad_request
+  end
 
-  def update; end
+  def recover_password
+    if recover_password_params[:password] != recover_password_params[:password_confirmation]
+      return render json: { error: 'As a senhas não coincidem!' }, status: :bad_request
+    end
+
+    @password_recovery = PasswordRecovery.find_by_token!(recover_password_params[:token])
+    @user = @password_recovery.user
+
+    @password_recovery.destroy! if @user.update(password: recover_password_params[:password])
+
+    render json: { valid: true, message: 'Senha alterada com sucesso!' }, status: :accepted
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Erro ao recuperar senha, tente novamente mais tarde' }, status: :bad_request
+  end
 
   private
 
   def user
     @user = User.find_by_email!(forgot_params[:email])
   rescue ActiveRecord::RecordNotFound
-    render json: { error: 'O e-mail informado não é válido!' }, status: :not_found
+    render json: { error: 'O e-mail informado não é válido!' }, status: :bad_request
   end
 
   def forgot_params
     params.require(:password).permit(
       :email
-    ).to_unsafe_h
+    ).to_unsafe_h.to_snake_keys.symbolize_keys
+  end
+
+  def verify_code_params
+    params.require(:passwordRecovery).permit(
+      :code
+    ).to_unsafe_h.to_snake_keys.symbolize_keys
+  end
+
+  def recover_password_params
+    params.require(:passwordRecovery).permit(
+      :token,
+      :password,
+      :passwordConfirmation
+    ).to_unsafe_h.to_snake_keys.symbolize_keys
   end
 end
