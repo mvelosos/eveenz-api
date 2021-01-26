fea768a233a1d9f79d70c0b850fe6191f4050bab
Mateus Veloso <mfvelosos@gmail.com>
2021-01-26 12:32:49 -0300

    created request_helper to support on requests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,16 @@ require 'simplecov'
 
 SimpleCov.start 'rails'
 
+Dir["#{File.dirname(__FILE__)}/../spec/support/*.rb"].sort.each { |f| require f }
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
+
+  config.include Requests::DisableFlashSweeping, type: :controller
+  config.include Requests::JsonHelpers, type: :controller
 
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
