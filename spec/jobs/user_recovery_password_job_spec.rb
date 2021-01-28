5c1261fb5f9f2aec7a8d50d6dc21ad2ea118f84d
Mateus Veloso <mfvelosos@gmail.com>
2021-01-28 01:39:08 -0300

    created spec for user_recovery_password_job

--- spec/jobs/user_recovery_password_job_spec.rb
+++ spec/jobs/user_recovery_password_job_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe UserRecoveryPasswordJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+  end
+
+  context '.perform' do
+    it 'should create a password recovery for the user' do
+      UserRecoveryPasswordJob.new.perform(@user.id)
+      expect(@user.password_recovery.present?).to eq(true)
+    end
+
+    it 'should trigger #send_recovery_code from PasswordsMailer' do
+      UserRecoveryPasswordJob.new.perform(@user.id)
+      expect { PasswordsMailer.send_recovery_code(@user).deliver_later }.to have_enqueued_job.on_queue('mailers')
+                                                                                             .with('PasswordsMailer',
+                                                                                                   'send_recovery_code',
+                                                                                                   'deliver_now',
+                                                                                                   @user)
+    end
+  end
 end
