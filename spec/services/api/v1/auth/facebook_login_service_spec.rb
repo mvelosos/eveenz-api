3f4aedc230b142b94ca54e5571ceec8718cd7fc8
Mateus Veloso <mfvelosos@gmail.com>
2021-01-29 10:15:28 -0300

    fixed faceboo login token spec

--- spec/services/api/v1/auth/facebook_login_service_spec.rb
+++ spec/services/api/v1/auth/facebook_login_service_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe Api::V1::Auth::FacebookLoginService, type: :service do
   before do
     User.destroy_all
     # This token was generate at Facebook Developers. Please, do not change.
-    @fb_access_token = 'EAAJXl0kUqRkBACGRlnvgZBopGyycUVZBak89BzIIU16IXuPnoujNPai9GzswRVyV1u4m4gZAcQTm0TgV1sZB7lb2sG3r4JZAZCRf'\
-                       'WYll2BO65fVUKLLK0pYRwdd4twTIbUK57g9QtwZCBJyfRBXpzlZAK2NJd0zYo20rbR7sJWo9MPl0lxKCPvRBdnWeola9V3bKezvd'\
-                       'Pj7WCQZDZD'.freeze
+    @fb_access_token = 'EAAJXl0kUqRkBAFbO8LfuZBup0DiMbR6SCnljqmLXSIiETRYHbNp0rXAaQxeRnaFgslwB6UJtHalvpmGdZBZBeCsLmwkuh497wZC'\
+                       '1rqymrCIzLg8FT8uSwDhKyqTv2fHNZB4ogKnTwhu6EV8R4cTbZAaxeP3cYJtOM4Xu19TmPn72E5tyy5gcoApiMbLoUTcOBe0VTzV'\
+                       'iIHLQZDZD'.freeze
   end
 
   context 'call facebook login service' do
