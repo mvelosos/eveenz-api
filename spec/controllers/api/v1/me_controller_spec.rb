c5915cf0585af4ea80c3239e3e56211ff6095b17
Mateus Veloso <mfvelosos@gmail.com>
2021-01-30 17:16:49 -0300

    fixed spec on me_controller

--- spec/controllers/api/v1/me_controller_spec.rb
+++ spec/controllers/api/v1/me_controller_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe Api::V1::MeController, type: :controller do
     context 'when user updates his own account with valid params' do
       it 'should return his own data' do
         get :update, params: { account: { name: 'foobar' } }
-        expect(json['me']['id']).to eq(@current_user.id)
+        expect(json['me']['id']).to eq(@current_user.account.id)
         expect(@current_user.account.name).to eq('foobar')
         expect(response).to have_http_status(:ok)
       end
