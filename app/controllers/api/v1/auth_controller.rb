89914fcfe5a2495d43edde02863faad5bde18c19
Mateus Veloso <mfvelosos@gmail.com>
2021-02-06 21:18:04 -0300

    created endpoint to google auth

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::AuthController < Api::V1::ApiController
-  before_action :authenticate_by_token, except: %i[login facebook]
+  before_action :authenticate_by_token, except: %i[login facebook google]
   before_action :find_by_username_or_email, only: %i[login]
 
   # POST /auth/login
@@ -25,6 +25,14 @@ class Api::V1::AuthController < Api::V1::ApiController
     render json: { errors: e.message }, status: :bad_request
   end
 
+  # POST /auth/google
+  def google
+    user = Api::V1::Auth::GoogleLoginService.call(google_params[:glAccessToken])
+    binding.pry
+
+    render json: 'workinggg', status: :ok
+  end
+
   private
 
   def find_by_username_or_email
@@ -43,6 +51,12 @@ class Api::V1::AuthController < Api::V1::ApiController
   def facebook_params
     params.require(:facebook).permit(
       :fbAccessToken
+    ).to_unsafe_h.to_snake_keys.symbolize_keys
+  end
+
+  def google_params
+    params.require(:google).permit(
+      :glAccessToken
     ).to_unsafe_h.to_snake_keys.symbolize_keys
   end
 end
