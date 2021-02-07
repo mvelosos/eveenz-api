e99120ad2262829798d0cfe62ed228501b8878a1
Mateus Veloso <mfvelosos@gmail.com>
2021-02-06 21:17:49 -0300

    created google login service

---
+++ app/services/api/v1/auth/google_login_service.rb
@@ -0,0 +1,48 @@
+class Api::V1::Auth::GoogleLoginService
+  def self.call(access_token)
+    obj = new(access_token)
+    obj.login
+  end
+
+  def initialize(access_token)
+    @access_token = access_token
+  end
+
+  def login
+    fb_user = Facebook.info(@access_token)
+    find_or_create_fb_user(fb_user)
+  end
+
+  private
+
+  def find_or_create_fb_user(fb_user)
+    @user = User.find_by_email(fb_user['email'])
+
+    unless @user.present?
+      user_params = {
+        username: generate_username_from_email(fb_user['email']),
+        email: fb_user['email'],
+        password: SecureRandom.hex(16)
+      }
+      @user = Api::V1::Users::NewUserService.call(user_params)
+      @user.provider = Settings.Providers.FACEBOOK
+    end
+
+    if @user.new_record?
+      @user.account.update(name: fb_user['name'])
+      @user.account.avatar.attach(io: fb_avatar(fb_user), filename: 'avatar')
+    end
+
+    @user.save!
+
+    @user
+  end
+
+  def generate_username_from_email(email)
+    "#{email.split('@')[0]}#{rand(9999)}"
+  end
+
+  def fb_avatar(fb_user)
+    URI.open("https://graph.facebook.com/#{fb_user['id']}/picture?height=500&width=500")
+  end
+end
