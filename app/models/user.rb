98aee359645f39bbf3c683b254679147290c07cd
Mateus Veloso <mfvelosos@gmail.com>
2021-02-08 16:43:47 -0300

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,6 +40,7 @@ class User < ApplicationRecord
   validates :email,    presence: true, uniqueness: true
   validates :email,    format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, length: { minimum: 6 }, if: -> { new_record? || !password.nil? }
+  validates :provider, presence: true
   validates :provider, inclusion: { in: PROVIDERS }
 
   validates_associated :account, on: :create
