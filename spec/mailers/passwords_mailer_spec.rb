2da956180599d7c8b98c26c40283ade3ecbe9d97
Mateus Veloso <mfvelosos@gmail.com>
2021-02-08 15:01:21 -0300

    implement passwords_mailer spec

--- spec/mailers/passwords_mailer_spec.rb
+++ spec/mailers/passwords_mailer_spec.rb
@@ -1,5 +1,24 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe PasswordsMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'send_recovery_code' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:mail) { PasswordsMailer.send_recovery_code(user).deliver_now }
+
+    before do
+      user.create_password_recovery!
+    end
+
+    it 'renders the subject' do
+      expect(mail.subject).to eq('Instruções para atualização de senha!')
+    end
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([user.email])
+    end
+
+    it 'renders the user recovery code' do
+      expect(mail.body.encoded).to match(user.password_recovery.code)
+    end
+  end
 end
