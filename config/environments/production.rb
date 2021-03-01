3e844e0016d5ebdc915586e2521835fe2365b1c1
Mateus Veloso <mfvelosos@gmail.com>
2021-03-01 19:31:38 -0300

    set mailgun production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,17 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  # Mailer service settings
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :address => ENV['MAILGUN_SMTP_SERVER'],
+    :port => ENV['MAILGUN_SMTP_PORT'],
+    :domain => ENV['MAILGUN_DOMAIN'],
+    :user_name => ENV['MAILGUN_SMTP_LOGIN'],
+    :password => ENV['MAILGUN_SMTP_PASSWORD']
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
