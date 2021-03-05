0d41c99521e6cbe884c30a706bb49ebaac0fff2f
Mateus Veloso <mfvelosos@gmail.com>
2021-03-05 01:15:24 -0300

    updated create_events migration

--- db/migrate/20190824210623_create_events.rb
+++ db/migrate/20190824210623_create_events.rb
@@ -11,6 +11,7 @@ class CreateEvents < ActiveRecord::Migration[5.2]
       t.date :end_date
       t.time :start_time
       t.time :end_time
+      t.boolean :undefined_end, default: false
       t.text :tags, array: true, :default => []
 
       t.timestamps
