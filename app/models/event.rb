92fbc1d967028374d32285cb866e38ffb3b818c6
Mateus Veloso <mfvelosos@gmail.com>
2021-03-05 01:14:33 -0300

    annotations and undefnied_end callback

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,21 +2,22 @@
 #
 # Table name: events
 #
-#  id           :bigint           not null, primary key
-#  uuid         :uuid             not null
-#  account_id   :bigint
-#  name         :string
-#  description  :text
-#  active       :boolean          default(TRUE)
-#  privacy      :string
-#  start_date   :date
-#  end_date     :date
-#  start_time   :time
-#  end_time     :time
-#  tags         :text             default([]), is an Array
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
-#  discarded_at :datetime
+#  id            :bigint           not null, primary key
+#  uuid          :uuid             not null
+#  account_id    :bigint
+#  name          :string
+#  description   :text
+#  active        :boolean          default(TRUE)
+#  privacy       :string
+#  start_date    :date
+#  end_date      :date
+#  start_time    :time
+#  end_time      :time
+#  undefined_end :boolean          default(FALSE)
+#  tags          :text             default([]), is an Array
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  discarded_at  :datetime
 #
 
 class Event < ApplicationRecord
@@ -45,12 +46,23 @@ class Event < ApplicationRecord
 
   has_many_base64_attached :images
 
-  validates :name,       presence: true
-  validates :start_date, presence: true
-  validates :start_time, presence: true
-  validates :privacy,    presence: true, inclusion: { in: PRIVACIES }
+  validates :name,          presence: true
+  validates :start_date,    presence: true
+  validates :start_time,    presence: true
+  validates :end_date,      presence: true, unless: -> { undefined_end? }
+  validates :end_time,      presence: true, unless: -> { undefined_end? }
+  validates :privacy,       presence: true, inclusion: { in: PRIVACIES }
+
+  before_create :check_undefined_end
 
   def search_data
     { name: name }
+  end
+
+  def check_undefined_end
+    return unless undefined_end
+
+    self.end_date = nil
+    self.end_time = nil
   end
 end
