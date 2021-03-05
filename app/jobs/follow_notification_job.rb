1c2b29aabc87df19ccf4dbf3caaa40e8bc01b5dd
Mateus Veloso <mfvelosos@gmail.com>
2021-03-05 01:24:51 -0300

    updated push message to follow notification

--- app/jobs/follow_notification_job.rb
+++ app/jobs/follow_notification_job.rb
@@ -2,8 +2,8 @@ class FollowNotificationJob < ApplicationJob
   queue_as :push_notifications
 
   def perform(follow)
-    title   = 'Novo seguidor'
-    content = "@#{follow.follower.user.username} está seguindo você!"
+    title   = ''
+    content = "#{follow.follower.name} (@#{follow.follower.user.username}) está seguindo você!"
     users   = [follow.followable.uuid]
 
     notification = Api::V1::Notifications::PushNotificationsService.new
