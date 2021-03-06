5a8956618db311d5ff25ac6ab334c02ad5942ccc
Mateus Veloso <mfvelosos@gmail.com>
2021-03-06 00:43:41 -0300

    created new mailer to notify user that his password was sucessfully updated

--- app/mailers/passwords_mailer.rb
+++ app/mailers/passwords_mailer.rb
@@ -6,4 +6,9 @@ class PasswordsMailer < ApplicationMailer
     @password_recovery = user.password_recovery
     mail(to: @user.email, subject: 'Instruções para recuperação de senha!')
   end
+
+  def password_successfully_updated(user)
+    @user = user
+    mail(to: @user.email, subject: 'Sua senha foi alterada com sucesso!')
+  end
 end
--- spec/mailers/passwords_mailer_spec.rb
+++ spec/mailers/passwords_mailer_spec.rb
@@ -20,5 +20,30 @@ RSpec.describe PasswordsMailer, type: :mailer do
     it 'renders the user recovery code' do
       expect(mail.body.encoded).to match(user.password_recovery.code)
     end
+
+    it 'renders the user username' do
+      expect(mail.body.encoded).to match(user.username)
+    end
+  end
+
+  describe 'password_successfully_updated' do
+    let(:user) { FactoryBot.create(:user) }
+    let(:mail) { PasswordsMailer.password_successfully_updated(user).deliver_now }
+
+    before do
+      user.update!(password: 'foobar123')
+    end
+
+    it 'renders the subject' do
+      expect(mail.subject).to eq('Sua senha foi alterada com sucesso!')
+    end
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eq([user.email])
+    end
+
+    it 'renders the user username' do
+      expect(mail.body.encoded).to match(user.username)
+    end
   end
 end
