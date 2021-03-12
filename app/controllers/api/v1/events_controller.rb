d82a496a799a174729b2aa556107e0ff0915958b
Mateus Veloso <mfvelosos@gmail.com>
2021-03-12 16:00:26 -0300

    updated events_controller to add update action

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::EventsController < Api::V1::ApiController
+  before_action :event, only: %i[update]
+
   # GET /events
   def index
     account = current_user.account
@@ -17,7 +19,21 @@ class Api::V1::EventsController < Api::V1::ApiController
     end
   end
 
+  # PUT/PATCH /events/:uuid
+  def update
+    return render json: { error: 'unauthorized' }, status: :unauthorized if @event.account_id != current_user.account.id
+
+    @event.update_attributes(event_params)
+    render json: @event, serializer: EventSerializer, account: current_user.account, status: :ok
+  end
+
   private
+
+  def event
+    @event = Event.find_by_uuid!(params[:uuid])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Event #{params[:uuid]} not found" }, status: :not_found
+  end
 
   def event_params
     params.require(:event).permit(
@@ -33,6 +49,7 @@ class Api::V1::EventsController < Api::V1::ApiController
       :privacy,
       addressAttributes: %i[street number neighborhood city state country zipCode],
       localizationAttributes: %i[latitude longitude],
+      eventCategoriesAttributes: %i[id categoryId _destroy],
       images: %i[data filename]
     ).to_unsafe_h.to_snake_keys
   end
