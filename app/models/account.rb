b438578bf850ff5428368037c48f46233f1c8dbc
Mateus Veloso <mfvelosos@gmail.com>
2021-06-26 01:06:43 -0300

    wip

--- app/models/account.rb
+++ app/models/account.rb
@@ -28,6 +28,7 @@ class Account < ApplicationRecord
 
   has_one_base64_attached :avatar
 
+  # accepts_nested_attributes_for :account_setting, update_only: true
   accepts_nested_attributes_for :user,            update_only: true
   accepts_nested_attributes_for :address,         update_only: true
   accepts_nested_attributes_for :localization,    update_only: true
