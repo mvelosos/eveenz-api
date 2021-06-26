c1d6b145d68897b1430c117c59fda02519881642
Mateus Veloso <mfvelosos@gmail.com>
2021-06-26 01:01:23 -0300

    remove account_settings nested attributes

--- app/models/account.rb
+++ app/models/account.rb
@@ -28,7 +28,6 @@ class Account < ApplicationRecord
 
   has_one_base64_attached :avatar
 
-  accepts_nested_attributes_for :account_setting, update_only: true
   accepts_nested_attributes_for :user,            update_only: true
   accepts_nested_attributes_for :address,         update_only: true
   accepts_nested_attributes_for :localization,    update_only: true
