6ea5b68c66f292ea06f1fa8c0065660dfcc2e5d9
Mateus Veloso <mfvelosos@gmail.com>
2021-06-25 23:28:11 -0300

    cop fixes

--- spec/jobs/follow_push_notification_job_spec.rb
+++ spec/jobs/follow_push_notification_job_spec.rb
@@ -5,11 +5,12 @@ RSpec.describe FollowPushNotificationJob, type: :job do
     before do
       @user1 = FactoryBot.create(:user)
       @user2 = FactoryBot.create(:user)
-      @follow = FactoryBot.create(:follow, followable: @user1, follower: @user2)
+      @follow = FactoryBot.create(:follow, followable: @user1.account, follower: @user2.account)
     end
 
     it 'send follow push notification' do
-      expect { FollowPushNotificationJob.perform_later(@follow) }.to have_enqueued_job.with(@follow).on_queue('push_notifications')
+      expect { FollowPushNotificationJob.perform_later(@follow) }.to have_enqueued_job
+        .with(@follow).on_queue('push_notifications')
     end
   end
 end
