a3d53082bd4e2aef5544865be61e938e317a3ed1
mvelosos <mfvelosos@gmail.com>
2021-08-02 22:37:16 -0300

    fix user spec facebook test token

--- spec/support/auth/facebook_test_token.rb
+++ spec/support/auth/facebook_test_token.rb
@@ -3,8 +3,8 @@ module Auth
     def retrieve_facebook_test_token
       client_id = '659257231190297'
       client_secret = '7e8fde7f90333cf761f109e9e5a78d91'
-      token = 'EAAJXl0kUqRkBAHCMiCh99oDtuyMTmd78bnY7RjVCJz5gJyOQbUwSRmJ9fpTZB1ix6JNMk8jqNcpnqwXtZBacKM90OwSjZCparPcpr6tb'\
-              'kyV3ioJKncilztN2Secoj9upzZAwkNYb9lfH9B9HlDFjZA8TehJZBqL4V96Dg50W2s7lOEW28SHHWH'
+      token = 'EAAJXl0kUqRkBAEIa8aFrEOtef7I1JcDeOnu3aW0CkZCyJ5smMv4TUAF8zvLsZC1L3H9mLqNZA9evZAkV28wRzkmJoeobZCBYZBGmdvy'\
+              'igVDMCYkcCzyGNR6HVWPaNnk9RGrQDdHrBgJZAB1zPzBqzRXzy1rZAws2n7MYAN7mNPARTL9RrBkoFv5g'
 
       url = "https://graph.facebook.com/v9.0/oauth/access_token?grant_type=fb_exchange_token&client_id=#{client_id}"\
             "&client_secret=#{client_secret}&fb_exchange_token=#{token}&auth_type=reauthorize"
@@ -16,7 +16,7 @@ module Auth
 end
 
 # Get long live auth_token
-# curl -i -X GET "https://graph.facebook.com/v10.0/oauth/access_token?grant_type=fb_exchange_token&client_id=659257231190297&client_secret=7e8fde7f90333cf761f109e9e5a78d91&fb_exchange_token=EAAJXl0kUqRkBAC4hqsIIh1sizOs9SaTpE7F1LLxdhAXcQdZB2VbLpVZCiFiURA6W79Koc8txknzZAJmviaFTVXOfZAaFRZCk0ezLEeXKdxCOTQ238gxXwx1klei6GddA2krEMiWCjubkqSos89JS7ZAZBxGYprYNxHrHvcCwryXk4JdBzPATEzs10Rof78C4aNAeOdBvF4nfAZDZD"
+# curl -i -X GET "https://graph.facebook.com/v10.0/oauth/access_token?grant_type=fb_exchange_token&client_id=659257231190297&client_secret=7e8fde7f90333cf761f109e9e5a78d91&fb_exchange_token=EAAJXl0kUqRkBAKhTZBjcXFu5CZC6ot00eSVP426Wa0wnXhGYvfEmpWpRQ9xd9bADAdIyypB12tzfGjDvQ1IZCJNkGeaqTyI7va2sczpKZClB5JN0wvhVaqzJZCEfvNZCGFSrZBnAawGR7mjCpEc3LZCzeJepGddG9AQY4EgSZBcH2MoMbc0uzoEVA02ZB0bMPGJf0Ap9oHdOLUMgZDZD"
 
 # Check user permissions
-# curl -i -X GET 'https://graph.facebook.com/me/permissions?access_token=EAAJXl0kUqRkBAC7sESmuzbNojLdWPKwHqoYMor7QJFGmovdZClspipcPwcl2VDthKTGFEtRCQ1sJmmvfnfRc7IZAZAfS5gbLiJAfD9BtpDLrPLnZCTW6ix0k4Of336NSHsthLzjchW8NwPEaZCQ4u1MTui8AZBNynI4cxegPDIm1XfmgJfCvmC&debug=all'
+# curl -i -X GET 'https://graph.facebook.com/me/permissions?access_token=EAAJXl0kUqRkBAEIa8aFrEOtef7I1JcDeOnu3aW0CkZCyJ5smMv4TUAF8zvLsZC1L3H9mLqNZA9evZAkV28wRzkmJoeobZCBYZBGmdvyigVDMCYkcCzyGNR6HVWPaNnk9RGrQDdHrBgJZAB1zPzBqzRXzy1rZAws2n7MYAN7mNPARTL9RrBkoFv5g&debug=all'
