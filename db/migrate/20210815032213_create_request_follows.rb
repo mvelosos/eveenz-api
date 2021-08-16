40ba553909628b613263c6b44c44260cf1eb73f6
mvelosos <mfvelosos@gmail.com>
2021-08-15 23:09:39 -0300

    update create_request_follows

--- db/migrate/20210815032213_create_request_follows.rb
+++ db/migrate/20210815032213_create_request_follows.rb
@@ -1,6 +1,7 @@
 class CreateRequestFollows < ActiveRecord::Migration[6.1]
   def change
     create_table :request_follows do |t|
+      t.uuid :uuid, null: false, unique: true, default: 'uuid_generate_v4()'
       t.references :requested_by, null: false, foreign_key: { to_table: :accounts }
       t.references :account, null: false, foreign_key: true
       t.boolean :accepted
