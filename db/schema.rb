8514075afc4339d840514c7d3b18b98c04687a20
mvelosos <mfvelosos@gmail.com>
2021-10-02 01:09:44 -0300

    add searchable_functions to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_191429) do
+ActiveRecord::Schema.define(version: 2021_10_02_040529) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
+  enable_extension "unaccent"
   enable_extension "uuid-ossp"
 
   create_table "account_settings", force: :cascade do |t|
