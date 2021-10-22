a0fb249567b5e659a626b06cdc9accab7ad81912
mvelosos <mfvelosos@gmail.com>
2021-10-22 01:50:23 -0300

    create cards_constructor_service to home_controller

--- spec/services/api/v1/home/cards_constructor_service_spec.rb
+++ spec/services/api/v1/home/cards_constructor_service_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::Events::NearEventsService, type: :service do
+RSpec.describe Api::V1::Home::CardsContructorService, type: :service do
   pending "add some examples to (or delete) #{__FILE__}"
 end
